861d76bf3a910ec20016a8a9565f94f60f501107
macbook <wkoua@oxylead.net>
2023-02-08 05:33:56 +0000

    commit

---
+++ src/main/java/com/dailychalleng/dailychallenge/Dailychallenge.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.dailychalleng.dailychallenge;
+
+/**
+ *
+ * @author Koua Wilfried
+ */
+public class Dailychallenge {
+
+    public static void main(String[] args) {
+        
+        DerivedClass derivedObject = new DerivedClass();
+        derivedObject.display();
+    
+    }
+}
